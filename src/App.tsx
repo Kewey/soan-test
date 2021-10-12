a4ec9d499cd8ae643d88d63c62725b6f9a048086
Jordan Souchez <jsouchez@feelity.fr>
2021-10-12 19:29:41 +0200

    :package: Add SCSS + RRD

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,60 @@
 // import { useState } from 'react'
-import { useFetch } from './hooks/useFetch'
+import './index.scss'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import lock_black_24dp from './assets/lock_black_24dp.svg'
-import PaymentItem from './ui/payment/paymentItem'
+import PaymentScreen from './ui/payment/paymentScreen'
+import { useFetch } from './hooks/useFetch'
 
 function App() {
-	// const [toggle, setToggle] = useState(false)
-
 	const { data, error, isLoading } = useFetch(
 		'https://test.soan-solutions.io/test_front/datas'
 	)
 
+	let payed = data?.payments?.filter((item: any) => {
+		if (item.payedDate === null) return null
+		return item
+	})
+
+	let waiting = data?.payments?.filter((item: any) => {
+		if (item.payedDate !== null) return null
+		return item
+	})
+
+	const totalAmount = waiting?.reduce((prev: number, current: any) => {
+		return prev + parseInt(current.amount)
+	}, 0)
+
+	console.log(`totalAmount`, totalAmount)
+
+	if (error) {
+		return <div>Error</div>
+	}
+
 	return (
-		<div className='container'>
-			<div className='card'>
-				<div className='card-head'>
-					<button>Factures à player</button>
-					<button>Facture payées</button>
-				</div>
-				<div className='card-body'>
+		<Router>
+			<div className='container'>
+				<div className='card'>
+					<div className='card-head'>
+						<Link to='/'>Factures à player</Link>
+						<Link to='/payed'>Facture payées</Link>
+					</div>
+
 					{isLoading && <p>Chargement</p>}
-
-					{error && <p>Oups, une erreur s'est produite</p>}
-
-					{data?.payments.map((payment: any) => {
-						return <PaymentItem payment={payment} />
-					})}
+					<Switch>
+						<Route path='/payed'>
+							<PaymentScreen list={payed} />
+						</Route>
+						<Route path='/'>
+							<PaymentScreen list={waiting} amount={totalAmount} />
+						</Route>
+					</Switch>
 				</div>
-				<div className='card-footer'>
-					<button className='btn'>Payer xxx €</button>
+				<div className='footer'>
+					<img src={lock_black_24dp} alt='icone paiement sécurisé' />
+					<p>Paiement en ligne 100 % sécurisé</p>
 				</div>
 			</div>
-			<div className='footer'>
-				<img src={lock_black_24dp} alt='icone paiement sécurisé' />
-				<p>Paiement en ligne 100 % sécurisé</p>
-			</div>
-		</div>
+		</Router>
 	)
 }
 
